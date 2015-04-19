37ab9e2683bcd09d4c734f3492ad658bb83c158a
Arslan Qadri <arslanqadri@Arslans-MacBook-Pro.local>
2015-04-20 00:18:51 +0530

    Fixed cross browser custom elements

---
+++ custom_elements_client_vulcanized.js
@@ -0,0 +1,14 @@
+
+  var importDoc = document.currentScript.ownerDocument;
+  if(!('import' in document.createElement('link'))) {
+    importDoc = document._currentScript.ownerDocument;
+  }
+  var proto = Object.create(HTMLElement.prototype, {
+    createdCallback: {
+      value : function() {
+        var template = importDoc.querySelector("#customElementTemplate");
+        this.createShadowRoot().appendChild(document.importNode(template.content, true));
+      }
+    }
+  });
+  document.registerElement("custom-element", {prototype: proto});
