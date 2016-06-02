52222905eb4bfd26473204a50c09f193af36d9de
Arslan Qadri <qadriarslan@gmail.com>
2016-06-02 22:50:29 +0530

    Child component.

--- reactjs/egghead/App.js
+++ reactjs/egghead/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Widget = (props) => {
+	return (
+		<div>
+			<b>Set Prop1: </b><input type="text"
+				value={props.prop1}
+				onChange={props.update} />
+			<h1>Prop1: {props.prop1}</h1>
+		</div>
+	);
+}
+
 class App extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			state1: 'This is my State1.',
+			state1: 'Default Value',
 			state2: 10
 		};
 	}
@@ -17,23 +28,19 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<Widget prop1={this.state.state1}
+					update={this.updateState1.bind(this)} />
+				<Widget prop1={this.state.state1}
+					update={this.updateState1.bind(this)} />
+				<Widget prop1={this.state.state1}
+					update={this.updateState1.bind(this)} />
 				<b>Set State1: </b><input type="text"
+					value={this.state.state1}
 					onChange={this.updateState1.bind(this)} />
 				<h1>State1: {this.state.state1}</h1>
-				<h1>Prop1: {this.props.prop1}</h1>
-				<h2>Prop2: {this.props.prop2}</h2>
 			</div>
 		);
 	}
-}
-
-App.propTypes = {
-	prop1: React.PropTypes.string,
-	prop2: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-	prop1: 'Hello World!!'
 }
 
 ReactDOM.render(
