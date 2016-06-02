bcdcbc9120e9207f9ccb17b188f44fa8e07fa917
Arslan Qadri <qadriarslan@gmail.com>
2016-06-02 22:25:30 +0530

    React State.

--- reactjs/egghead/App.js
+++ reactjs/egghead/App.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			state1: 'This is my State1.',
+			state2: 10
+		};
+	}
+	updateState1(e) {
+		this.setState({
+			state1: e.target.value
+		})
+	}
 	render() {
 		return (
 			<div>
+				<b>Set State1: </b><input type="text"
+					onChange={this.updateState1.bind(this)} />
+				<h1>State1: {this.state.state1}</h1>
 				<h1>Prop1: {this.props.prop1}</h1>
 				<h2>Prop2: {this.props.prop2}</h2>
 			</div>
