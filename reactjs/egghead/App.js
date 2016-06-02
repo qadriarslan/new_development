f8bc674f30e348ac48371208e0311a0d5d4889ae
Arslan Qadri <qadriarslan@gmail.com>
2016-06-02 10:56:09 +0530

    Moving rendering to App from main.js.

--- reactjs/egghead/App.js
+++ reactjs/egghead/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
-				Hello World!!
-				<Me />
+				<h1>Hello World!!</h1>
 			</div>
 		);
 	}
 }
 
-const Me = () => <h1>Hello to you</h1>
+//const Me = () => <h1>Hello to you</h1>
 
+ReactDOM.render(<App />, document.getElementById('app'));
 export default App
