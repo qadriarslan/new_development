b7f977560f13b36d8b61c23633067efe9739142f
Arslan Qadri <qadriarslan@gmail.com>
2017-03-18 13:17:33 +0530

    Created basic friends list app.

--- reactjs/react-mine-basic/app/App.jsx
+++ reactjs/react-mine-basic/app/App.jsx
@@ -4,30 +4,96 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            username: "qadriarslan"
+            name: "Arslan Qadri",
+            profession: "SDE",
+            friends: []
         };
     }
-    handleChange(e) {
-        this.setState({username: e.target.value})
+
+    addFriend(friend) {
+        this.setState({friends: this.state.friends.concat(friend)})
     }
+
+    changeProfession(newProfession) {
+        this.setState({profession: newProfession})
+    }
+
     render() {
         return (
             <div>
-                <Greeter username={this.state.username}></Greeter>
-                Change Name: <input type="text" value={this.state.username} onChange={this.handleChange.bind(this)}/>
+                <PersonInfo name={this.state.name} profession={this.state.profession}></PersonInfo>
+                <FriendsList friends={this.state.friends}></FriendsList>
+                <AddFriends addFriend={this.addFriend.bind(this)}></AddFriends>
             </div>
         );
     }
 }
 
-class Greeter extends React.Component {
+class PersonInfo extends React.Component {
     render() {
         return (
             <div>
-                <p>Hello <span style={{color:'red'}}>{this.props.username}</span>!! How are you today??</p>
+                <p>Name: {this.props.name}</p>
+                <p>Profession: {this.props.profession}</p>
             </div>
         );
     }
 }
+
+class FriendsList extends React.Component {
+    render() {
+        let friends = this.props.friends.map(function(friend, counter) {
+            return <p key={counter}>Name: {friend.name}, Profession: {friend.profession}</p>
+        })
+        return (
+            <div>
+                Friends: [
+                {friends}
+                ]
+            </div>
+        );
+    }
+}
+
+FriendsList.defaultProps = {
+    friends: []
+};
+
+class AddFriends extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name: "",
+            profession: ""
+        };
+    }
+
+    handleNameChange(e) {
+        this.setState({name: e.target.value})
+    }
+
+    handleProfessionChange(e) {
+        this.setState({profession: e.target.value})
+    }
+
+    handleClick() {
+        this.props.addFriend({name: this.state.name, profession: this.state.profession});
+        this.setState({name: "",profession: ""});
+    }
+
+    render() {
+        return (
+            <div>
+                <input type="text" value={this.state.name} placeholder="Name" onChange={this.handleNameChange.bind(this)}/>
+                <input type="text" value={this.state.profession} placeholder="Profession" onChange={this.handleProfessionChange.bind(this)}/>
+                <button onClick={this.handleClick.bind(this)}>Add Friend</button>
+            </div>
+        )
+    }
+}
+
+AddFriends.propTypes = {
+    addFriend: React.PropTypes.func.isRequired
+};
 
 export default App
