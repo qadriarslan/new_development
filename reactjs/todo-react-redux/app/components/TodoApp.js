7e3c2165a1ab816c63026def44720c3b3f4f6491
Arslan Qadri <qadriarslan@gmail.com>
2017-08-15 23:46:22 +0530

    Made TodoList a presentation component.

--- reactjs/todo-react-redux/app/components/TodoApp.js
+++ reactjs/todo-react-redux/app/components/TodoApp.js
@@ -3,16 +3,35 @@ import { FILTERS, ACTIONS } from '../util/constants';
 import { AddTodo } from './AddTodo';
 import { TodoList } from './TodoList';
 import { TodoFilters } from './TodoFilters';
+import { getVisibleTodos } from '../util/todo-util';
 
 class TodoApp extends React.Component {
   render() {
     const { store } = this.props;
-    const state = store.getState();
+    const todos = store.getState().todos;
+    const filter = store.getState().filter;
+    const visibleTodos = getVisibleTodos(todos, filter);
+    const onTodoClick = todoId =>
+      store.dispatch({
+        type: ACTIONS.TOGGLE_TODO,
+        id: todoId
+      });
+
+    const onTodoDelete = todoId => (
+      store.dispatch({
+        type: ACTIONS.REMOVE_TODO,
+        id: todoId
+      })
+    );
+
     return (
       <div>
         <h2>Todo List</h2>
         <AddTodo store={store}/>
-        <TodoList store={store}/>
+        <TodoList
+          todos={visibleTodos}
+          onTodoClick={onTodoClick}
+          onTodoDelete={onTodoDelete} />
         <TodoFilters store={store}/>
       </div>
     );
--- reactjs/todo-react-redux/app/components/TodoList.js
+++ reactjs/todo-react-redux/app/components/TodoList.js
@@ -1,41 +1,19 @@
 import React from 'react';
-import { ACTIONS } from '../util/constants';
-import { getVisibleTodos } from '../util/todo-util';
 import { TodoItem } from './TodoItem';
 
-class TodoList extends React.Component {
-  onTodoClick(todoId) {
-    const { store } = this.props;
-    store.dispatch({
-      type: ACTIONS.TOGGLE_TODO, 
-      id: todoId
-    });
-  }
-  onTodoDelete(todoId) {
-    const { store } = this.props;
-    store.dispatch({
-      type: ACTIONS.REMOVE_TODO, 
-      id: todoId
-    });
-  }
-  render() {
-    const { store } = this.props;
-    const todos = store.getState().todos;
-    const filter = store.getState().filter;
-    const visibleTodos = getVisibleTodos(todos, filter)
-      .map(todo =>
-        <TodoItem 
-          key={todo.id} 
-          todo={todo}
-          onClick={this.onTodoClick.bind(this)}
-          onDelete={this.onTodoDelete.bind(this)} />
-      );
-
-    return (
-      <ul>
-        {visibleTodos}
-      </ul>
-    );
-  }
-}
+const TodoList = ({
+  todos,
+  onTodoClick,
+  onTodoDelete
+}) => (
+  <ul>
+    {todos.map(todo =>
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        onClick={onTodoClick}
+        onDelete={onTodoDelete} />
+    )}
+  </ul>
+);
 export { TodoList };
